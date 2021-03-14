7e8b09842416e2c44ba715cae51473763ffad039
EVA BARRERO <evabarreros@gmail.com>
2021-03-14 17:02:47 +0100

    arreglo tab precios

--- server.R
+++ server.R
@@ -170,6 +170,9 @@ shinyServer(function(input, output) {
   })
   
   output$preciosAndalucia <- renderPlotly({
+    
+    validate(need(input$selectAndalucia, ""))
+    
     andalucia %>%
       filter(SUBSECTOR %in% input$selectAndalucia) %>%
       group_by(SUBSECTOR, INICIO) %>%
@@ -193,6 +196,9 @@ shinyServer(function(input, output) {
   })
   
   output$preciosMadrid <- renderPlotly({
+    
+    validate(need(input$selectMadrid, ""))
+    
     mercaMadrid %>% 
       filter(familia %in% input$selectMadrid) %>%
       group_by(familia, Fecha) %>% 
@@ -207,8 +213,8 @@ shinyServer(function(input, output) {
       labs(x='Fecha', y='Precio medio', title='Precio medio por familias')
   })
   
-  output$selectizeBarna <- renderUI({
-    selectInput('selectBarna', 'Selecciona las familias', 
+  output$selectBarna <- renderUI({
+    selectizeInput('selectBarna', 'Selecciona las familias', 
                 choices = unique(mercaBarna$familia), 
                 selected = c('FRUTAS CÍTRICOS', 'FRUTAS HUESO', 'FRUTAS SEMILLA',
                              'HORTALIZAS BULBOS', 'HORTALIZAS FRUTO', 'HORTALIZAS INFLORESC.',
@@ -217,6 +223,9 @@ shinyServer(function(input, output) {
   })
   
   output$preciosBarna <- renderPlotly({
+    
+    validate(need(input$selectBarna, ""))
+    
     mercaBarna %>% 
       filter(familia %in% input$selectBarna) %>%
       group_by(familia, Fecha) %>% 
